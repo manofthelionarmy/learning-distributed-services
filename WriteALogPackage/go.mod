d4b6a163fc696ac0bee2d0ef8bdf55023201cc26
Armando Leon <aleon45@ucmerced.edu>
2021-11-09 23:59:48 -0700

    Added metrics, tracing, and logging capabilities

--- WriteALogPackage/go.mod
+++ WriteALogPackage/go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/stretchr/testify v1.7.0
 	github.com/tysonmote/gommap v0.0.0-20210506040252-ef38c88b18e1
+	go.opencensus.io v0.22.2
+	go.uber.org/zap v1.10.0
 	google.golang.org/genproto v0.0.0-20200423170343-7949de9c1215
 	google.golang.org/grpc v1.32.0
 	google.golang.org/protobuf v1.27.1
--- WriteALogPackage/internal/server/server.go
+++ WriteALogPackage/internal/server/server.go
@@ -2,9 +2,19 @@ package server
 
 import (
 	"context"
+	"time"
+
+	"go.opencensus.io/plugin/ocgrpc"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/trace"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	api "github.com/manofthelionarmy/prolog/api/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -43,18 +53,47 @@ type grpcServer struct {
 }
 
 func NewGRPCServer(config *Config, opts ...grpc.ServerOption) (*grpc.Server, error) {
+	logger := zap.L().Named("server")
+	zapOpts := []grpc_zap.Option{
+		grpc_zap.WithDurationField(
+			func(duration time.Duration) zapcore.Field {
+				return zap.Int64(
+					"grpc.time_ns",
+					duration.Nanoseconds(),
+				)
+			},
+		),
+	}
+	// halfSampler := trace.ProbabilitySampler(0.5)
+	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
+	// trace.ApplyConfig(trace.Config{DefaultSampler: func(p trace.SamplingParameters) trace.SamplingDecision {
+	// 	if strings.Contains(p.Name, "Produce") {
+	// 		return trace.SamplingDecision{Sample: true}
+	// 	}
+	// 	return halfSampler(p)
+	// }})
+	err := view.Register(ocgrpc.DefaultServerViews...)
+	if err != nil {
+		return nil, err
+	}
 	// wtf is this?! the code is hard to follow 😔
 	// I think I cleaned it up
 	opts = append(opts,
 		// an individual server option
 		grpc.StreamInterceptor(
 			grpc_middleware.ChainStreamServer(
+				grpc_ctxtags.StreamServerInterceptor(),
+				grpc_zap.StreamServerInterceptor(logger, zapOpts...),
 				grpc_auth.StreamServerInterceptor(authenticate),
 			)),
 		// an individual server option
 		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpc_ctxtags.UnaryServerInterceptor(),
+			grpc_zap.UnaryServerInterceptor(logger, zapOpts...),
 			grpc_auth.UnaryServerInterceptor(authenticate),
-		)))
+		)),
+		grpc.StatsHandler(&ocgrpc.ServerHandler{}),
+	)
 	gsrv := grpc.NewServer(opts...)
 	srv, err := newgrpcServer(config)
 	if err != nil {
